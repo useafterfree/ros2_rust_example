19eed4c004f33551cb67e0afd666694a4e14150c
Nathan Trujillo <35079898+useafterfree@users.noreply.github.com>
2025-09-29 10:26:31 -0700

    Latest

---
+++ notes.sh
@@ -0,0 +1,16 @@
+screen -dmS foxglove ros2 launch foxglove_bridge foxglove_bridge_launch.xml;
+ros2 run rust_example udppublisher;
+gst-launch-1.0 udpsrc port=5000   ! application/x-rtp, media=video, clock-rate=90000, encoding-name=H264   ! rtph264depay   ! h264parse   ! avdec_h264   ! videoconvert   ! jpegenc   ! image/jpeg,format=jpeg   ! appsink name=sink emit-signals=false sync=false max-buffers=1 drop=true
+
+docker run \
+  --platform linux/amd64 \
+  -it \
+  -p 50001:5000/udp \
+  -p 8765:8765 \
+  --name ros2_container2 \
+  --env="DISPLAY=host.docker.internal:0" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v ~/ros2_ws:/root/ros2_ws \
+  localhost:50000/ros2-rust:latest
+
+  docker exec -it ros2_container2 bash
